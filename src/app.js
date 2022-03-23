39d47ba0f00477f22cab3a16fc414d180b6e564c
saravananp <saravananp@techaffinity.com>
2022-03-23 19:19:56 +0530

    ddd

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const hbs = require('hbs')
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewspath = path.join(__dirname, '../templates/views')
 const partialspath = path.join(__dirname, '../templates/partials')
@@ -30,7 +31,7 @@ app.get('/products', (req, res) => {
 app.get('/weather', (req, res) => {
     res.send('rainy')
 })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('port is running 3000')
 })
 // console.log('port is running 3000')
